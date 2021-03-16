a374696eaa63bd961d3d5893af5cfa6bc9f18b1c
Samuel Hegner <samuel.hegner@gmail.com>
2021-03-16 20:19:02 +0000

    Basic Bot

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  if message.content.startswith("&hello"):
+    await message.channel.send("OOOH OOH Ah!")
+
+
+client.run()
