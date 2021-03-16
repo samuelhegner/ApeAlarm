0fc4d96a5ecd42a2dfc2c8a5a5e0c95da13094f9
Samuel Hegner <samuel.hegner@gmail.com>
2021-03-16 21:05:55 +0000

    Continuous Running

--- main.py
+++ main.py
@@ -2,34 +2,38 @@ import discord
 import os
 import time
 
+from keep_alive import keep_alive
+
 
 class MyClient(discord.Client):
   async def on_ready(self):
     print("Logged in as {0.user}".format(client))
 
+  
+  # Apes ID = 800378826630365225
 
   async def on_message(self, message):
     if message.author == client.user:
       return
   
     if message.content.startswith("&hello"):
-      await message.reply("OOOH OOH Ah! Hello {0.user}".format(client), )
+      await message.reply("OOOH OOH Ah! Hello {0.user}".format(client))
     
 
     if message.content.startswith("&league"):
       await message.channel.send("OOOH OOH Ah!")
       time.sleep(0.5)
       await message.channel.send("WEEEE OOOOH WEEEE OOOOH!")
-      time.sleep(0.1)
-      await message.channel.send("Attention all @apes")
-      time.sleep(0.1)
+      time.sleep(0.2)
+      await message.channel.send("Attention all <@&800378826630365225>")
+      time.sleep(0.2)
       await message.channel.send("LEAGUE NOW! ITS A GREAT GAME!")
 
 
     if message.content.startswith("&bloons"):
       await message.channel.send("OOOH OOH Ah!")
       time.sleep(0.5)
-      await message.channel.send("Any @apes, that are also Bloonsers?!")
+      await message.channel.send("Any <@&800378826630365225>, that are also Bloonsers?!")
 
 
 
@@ -37,5 +41,5 @@ client = MyClient()
 
 
 
-
+keep_alive()
 client.run(os.getenv("TOKEN"))
