3ee0774c3f5128ee39d1b38edce13c968dcae9e8
Samuel Hegner <samuel.hegner@gmail.com>
2021-03-16 20:41:00 +0000

    Basic Functionality

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import discord
+import os
+import time
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-  print('Logged in as {0.user}'.format(client))
+class MyClient(discord.Client):
+  async def on_ready(self):
+    print("Logged in as {0.user}".format(client))
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+
+  async def on_message(self, message):
+    if message.author == client.user:
+      return
   
-  if message.content.startswith("&hello"):
-    await message.channel.send("OOOH OOH Ah!")
+    if message.content.startswith("&hello"):
+      await message.reply("OOOH OOH Ah! Hello {0.user}".format(client), )
+    
+
+    if message.content.startswith("&league"):
+      await message.channel.send("OOOH OOH Ah!")
+      time.sleep(0.5)
+      await message.channel.send("WEEEE OOOOH WEEEE OOOOH!")
+      time.sleep(0.1)
+      await message.channel.send("Attention all @apes")
+      time.sleep(0.1)
+      await message.channel.send("LEAGUE NOW! ITS A GREAT GAME!")
 
 
-client.run()
+    if message.content.startswith("&bloons"):
+      await message.channel.send("OOOH OOH Ah!")
+      time.sleep(0.5)
+      await message.channel.send("Any @apes, that are also Bloonsers?!")
+
+
+
+client = MyClient()
+
+
+
+
+client.run(os.getenv("TOKEN"))
